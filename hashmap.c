8b3dc34abfa4ad1d54a9701515272a546526657d
tonto <151060549+aminoGOD@users.noreply.github.com>
2024-04-24 21:39:33 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,6 +40,18 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
+  if(map == NULL || key == NULL) return;
+  if(map->size == map->capacity) enlarge(map);
+  long posicion = hash(key,map->capacity);
+  while(map->buckets[posicion] != NULL && map->buckets[posicion]->key != NULL){
+    if(is_equal(map->buckets[posicion]->key,key)){
+      map->buckets[posicion]->value = value;
+      return;
+    }
+    posicion = (posicion+1) % map->capacity;
+    
+  }
+  
 
 
 }
