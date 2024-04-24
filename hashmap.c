3759fd565b3442dfc7dbf2c139b8eb8b423ecd62
tonto <151060549+aminoGOD@users.noreply.github.com>
2024-04-24 22:01:51 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -122,6 +122,15 @@ Pair * firstMap(HashMap * map) {
 }
 
 Pair * nextMap(HashMap * map) {
+  if(map == NULL) return NULL;
+  if(map->current == -1) return NULL;
+
+  for(int i = map->current + 1; i < map->capacity; i++){
+    if(map->buckets[i] != NULL && map->buckets[i]->key != NULL){
+      map->current = i;
+      return map->buckets[i];
+    }
+  }
 
     return NULL;
 }
